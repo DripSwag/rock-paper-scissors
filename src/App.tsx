4c2876282f30f5b4d00e3b3b5f46e37e848548a4
DripSwag <mimirani33@gmail.com>
2023-11-14 21:16:47 +0000

    Made rock button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
+import Rock from './assets/icon-rock.svg'
+
 function App() {
   return (
-    <main className="w-screen h-screen bg-background text-text text-white font-text">
-      <div className="p-8 flex flex-col justify-between h-screen">
-        <section className="border-border border-2 p-4 rounded-lg flex justify-between min-h-[8em] items-center">
-          <h1 className="leading-[0.75em] font-bold text-2xl">ROCK<br></br>PAPER<br></br>SCISSORS</h1>
-          <div className="h-full aspect-square bg-white rounded-lg flex flex-col items-center p-2 drop-shadow-lg">
-            <h2 className="text-score-heading tracking-widest text-sm">SCORE</h2>
-            <h2 className="text-score-value text-5xl font-extrabold">12</h2>
+    <main className='w-screen h-screen bg-gradient-to-b from-background-light to-background-dark text-text text-white font-text'>
+      <div className='p-8 flex flex-col justify-between items-center h-screen'>
+        <section className='border-border border-2 p-4 rounded-lg flex justify-between min-h-[8em] items-center w-full'>
+          <h1 className='leading-[0.75em] font-bold text-2xl'>
+            ROCK<br></br>PAPER<br></br>SCISSORS
+          </h1>
+          <div className='h-full aspect-square bg-white rounded-lg flex flex-col items-center p-2 drop-shadow-lg'>
+            <h2 className='text-score-heading tracking-widest text-sm'>
+              SCORE
+            </h2>
+            <h2 className='text-score-value text-5xl font-extrabold'>12</h2>
           </div>
         </section>
-        <section>
+        <section className='relative z-10'>
+          <div className='bg-white rounded-full aspect-square w-32 border-rock-light relative circleShadow before:bg-rock-dark'>
+            <img
+              src={Rock}
+              className='relative left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 aspect-square w-1/3'
+            ></img>
+            <div className='w-full h-full bg-transparent rounded-full absolute top-0 border-[16px] border-rock-light'></div>
+          </div>
         </section>
-        <button>
+        <button className='border-2 border-border w-min py-2 px-10 rounded-xl'>
           RULES
         </button>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        background: 'hsl(237, 49%, 15%)',
+        'background-light': 'hsl(214, 47%, 23%)',
+        'background-dark': 'hsl(237, 49%, 15%)',
         border: 'hsl(217, 16%, 45%)',
         'score-heading': 'hsl(229, 64%, 46%)',
-        'score-value': 'hsl(229, 25%, 31%)'
+        'score-value': 'hsl(229, 25%, 31%)',
+        'rock-dark': 'hsl(349, 71%, 52%)',
+        'rock-light': 'hsl(349, 70%, 56%)',
       },
       fontFamily: {
-        'text': ['"Barlow Semi Condensed"']
-      }
+        text: ['"Barlow Semi Condensed"'],
+      },
     },
   },
   plugins: [],
 }
-
